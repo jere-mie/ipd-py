58118e2ca2de709152581e0a9a241c817a73734d
Jeremie Bornais <jeremiejbornais@gmail.com>
2023-02-09 21:57:27 +0000

    next_move function working in all cases now

--- player_utils.py
+++ player_utils.py
@@ -1,3 +1,4 @@
+import random
 # a dict of pre-made strategy encodings
 known_strategies = {
     "ALLD_1":"00000",
@@ -17,7 +18,25 @@ known_strategies = {
     "STFT_3":"0101010101010101010101010101010101010101010101010101010101010101010101010101010101010"
 }
 
+# k-v pairs where the key is the encoding length and the value is the respective memory depth
+memory_depths = {
+    1:0,
+    5:1,
+    21:2,
+    85:3,
+    341:4,
+    1365:5
+}
+
 def next_move(encoding: str, memory: str) -> str:
+    # always return the last character when we have no memory
+    if len(memory) == 0:
+        return encoding[-1]
+
+    # if we have less memory than our memorydepth, but greater than no memory,
+    # we remove the main cases from the encoding to return a simpler encoding
+    if len(memory)//2 < memory_depths[len(encoding)]:
+        return next_move(encoding[-1*(len(encoding)//4):], memory)
     return encoding[int(memory, 2)]
 
 def encoding_length(memoryDepth=1) -> int:
@@ -30,13 +49,11 @@ def generate_strategies(numOfStrats, memoryDepth=1) -> list:
     strategies = []
     for _ in range(numOfStrats):
         strategies.append("".join([random.choice(['0', '1']) for __ in range(encoding_length(memoryDepth))]))
-        
     return strategies
 
 if __name__ == '__main__':
-    print(next_move(known_strategies['ALLD_1'], '00'))
-    print(next_move(known_strategies['ALLD_1'], '01'))
-    print(next_move(known_strategies['ALLC_1'], '00'))
-    print(next_move(known_strategies['ALLC_1'], '01'))
-    print(next_move(known_strategies['TFT_1'], '00'))
-    print(next_move(known_strategies['TFT_1'], '01'))
+    print(next_move(known_strategies['TFT_2'], '0000'))
+    print(next_move(known_strategies['TFT_2'], '00'))
+    print(next_move(known_strategies['TFT_2'], '0001'))
+    print(next_move(known_strategies['TFT_2'], '01'))
+    print(next_move(known_strategies['TFT_2'], ''))
